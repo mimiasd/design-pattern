616a93c430e9be1e5a772126943eb501a0a4e499
yuanjin <yuanjin@nibirutech.com>
2021-08-27 17:52:07 +0800

    增加: 装饰器模式

---
+++ 07_decorator/decorator_test.go
@@ -0,0 +1,12 @@
+package decorator
+
+import "testing"
+
+func TestDecorator(t *testing.T) {
+	var c Component = &ConcreteComponent{}
+	c = WarpAddDecorator(c, 10)
+	c = WarpMulDecorator(c, 8)
+	res := c.Calc()
+
+	t.Logf("res: %d\n", res)	
+}

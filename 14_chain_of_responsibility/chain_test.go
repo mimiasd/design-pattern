2a6fcd8d8d9fa587573e41bb13188e8f29518577
yuanjin <yuanjin@nibirutech.com>
2021-08-31 10:42:06 +0800

    增加: 责任链模式

---
+++ 14_chain_of_responsibility/chain_test.go
@@ -0,0 +1,18 @@
+package chain
+
+import "testing"
+
+func TestChain(t *testing.T) {
+	c1 := NewProjectManagerChain()
+	c2 := NewDepManagerChain()
+	c3 := NewGeneralManagerChain()
+
+	c1.SetSuccessor(c2)
+	c2.SetSuccessor(c3)
+
+	var c Manager = c1
+	c.HandleFeeRequest("bob", 400)
+	c.HandleFeeRequest("tom", 1400)
+	c.HandleFeeRequest("ada", 10000)
+	c.HandleFeeRequest("floar", 400)
+}
